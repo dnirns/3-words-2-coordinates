efc42bd32f72201e8d6ece43697a818a6c60746e
dnirns <daniel.irons@gmail.com>
2021-05-18 17:37:27 +0100

    simplified component using autosuggest input

--- src/components/Converter.js
+++ src/components/Converter.js
@@ -1,63 +1,46 @@
-import { useState } from 'react'
-import axios from 'axios'
+import { useState, useEffect } from 'react'
 
 const Converter = () => {
   const [words, setWords] = useState('')
   const [lat, setLat] = useState('')
   const [lng, setLng] = useState('')
-  const [location, setLocation] = useState({})
-  const [error, setError] = useState({})
 
-  const convertWords = async () => {
-    try {
-      const res = await axios.get(
-        `https://api.what3words.com/v3/convert-to-coordinates?words=${words}&key=${process.env.REACT_APP_WORDS_API}`
-      )
-      const data = res.data
-      setLat(data.coordinates.lat)
-      setLng(data.coordinates.lng)
-      setLocation(data)
-      setError({})
-    } catch (err) {
-      const res = err.response.data
-      setError(res.error)
-      setLocation({})
+  useEffect(() => {
+    const input = document.getElementById('autosuggest')
+    if (input) {
+      input.addEventListener('select', (value) => {
+        setWords(value.detail)
+        setLat(value.target.coordinatesLat)
+        setLng(value.target.coordinatesLng)
+      })
     }
-  }
+  })
+
   return (
-    <div className='container mx-auto text-center'>
-      <div>
-        {/* autocomplete input from w3w api docs */}
+    <div className='container mx-auto text-center py-10'>
+      <div className=''>
         <what3words-autosuggest
           id='autosuggest'
+          debug={true}
+          return-coordinates={true}
           placeholder='e.g. daring.lion.race'
         />
-      </div>{' '}
-      <button
-        className='my-6 rounded px-4 py-2 bg-green-400 text-white hover:bg-opacity-90'
-        onClick={convertWords}
-      >
-        GET COORDINATES
-      </button>
-      {location.coordinates && (
-        <div>
-          <p className='font-bold text-lg py-4'>
-            {lat},{lng}
-          </p>
+      </div>
 
-          <p>
-            {location.nearestPlace}, {location.country}
-          </p>
-          <a href={location.map}>
-            <p className='py-6 italic font-bold text-green-400 hover:text-green-300'>
-              {location.words}
+      <div>
+        {words && (
+          <>
+            <p className='py-6'>
+              Latitude / Longitude for <em className='font-bold'>what3words</em>{' '}
+              address
+              <span className='text-red-400 italic font-bold'>"{words}"</span>
             </p>
-          </a>
-        </div>
-      )}
-      {error.message && (
-        <p className='py-10 text-red-500'>Error: {error.message}</p>
-      )}
+            <p className='font-bold text-4xl py-4 italic'>
+              {lat}, {lng}
+            </p>
+          </>
+        )}
+      </div>
     </div>
   )
 }
